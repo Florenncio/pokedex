7f54147f9e674a2f2c06183e90122a386701ec4e
Fernando Florencio <nandoflorencio13@gmail.com>
2021-12-22 06:21:41 -0300

    #2

--- lib/features/pokedex/screens/home/pages/home_page.dart
+++ lib/features/pokedex/screens/home/pages/home_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/common/models/pokemon.dart';
+import 'package:pokedex/features/pokedex/screens/details/container/detail_continer.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key, required this.list}) : super(key: key);
+  const HomePage({Key? key, required this.list, required this.OnItemTap})
+      : super(key: key);
   final List<Pokemon> list;
+  final Function(String, DetailArguments) OnItemTap;
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +16,10 @@ class HomePage extends StatelessWidget {
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
               title: Text(list[index].name),
+              onTap: () => OnItemTap(
+                '/details',
+                DetailArguments(name: list[index].name),
+              ),
             );
           }),
     );
